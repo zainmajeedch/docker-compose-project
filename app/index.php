eff51ace6fa49463ab60b26c53df9bb794fff8b1
zain <zainch3390@gmail.com>
2026-08-04 17:22:33 +0500

    Initial commit: Docker Compose project with Nginx, PHP, MariaDB, Redis, and Worker

---
+++ app/index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Docker Compose</h1>";
+
+echo "<p>Nginx working</p>";
+
+echo "<p>PHP Version: " . phpversion() . "</p>";
---
+++ worker/worker.php
@@ -0,0 +1,6 @@
+<?php
+
+while (true) {
+    echo "[" . date("H:i:s") . "] Worker is running..." . PHP_EOL;
+    sleep(5);
+}
